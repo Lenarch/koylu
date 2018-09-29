e50dc31f7bcd136b10fede2204ae8357434a54f8
Lenarch <43513762+Lenarch@users.noreply.github.com>
2018-09-29 18:37:25 +0300

    Update koylu.js

--- koylu.js
+++ koylu.js
@@ -18,10 +18,6 @@ client.on('ready', () => {
     msg.channel.send('Selam Anatomy Ailesi.');
   }
 
-  if (msg.content === 'selam') {
-    msg.channel.send('Selam.');
-  }
-
 });
 client.on('message', message => {
   if (!message.guild) return;
@@ -69,24 +65,14 @@ client.on('message', message => {
     }
   }
 });
-client.on('message', message => {
-  // Voice only works in guilds, if the message does not come from a guild,
-  // we ignore it
-  if (!message.guild) return;
-
-  if (message.content === '/join') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => {// Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
-  }
-});
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
 
 
 client.login(process.env.BOT_TOKEN);
