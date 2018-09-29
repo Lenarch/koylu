2f7c22f95c491fa3a2c17ed71c26c29c1279e889
Lenarch <43513762+Lenarch@users.noreply.github.com>
2018-09-29 20:23:30 +0300

    Update koylu.js

--- koylu.js
+++ koylu.js
@@ -73,5 +73,11 @@ client.on('message', message => {
         message.channel.send(attachment);
     }
 });
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find('name', 'member-log');
+  if (!channel) return;
+  channel.send(`Welcome to the server, ${member}`);
+});
+
 
 client.login(process.env.BOT_TOKEN);
