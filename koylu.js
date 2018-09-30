574eb090ec7f8c0a2f3962bc15b793176c4a6b6f
Lenarch <43513762+Lenarch@users.noreply.github.com>
2018-09-30 14:09:54 +0300

    Update koylu.js

--- koylu.js
+++ koylu.js
@@ -73,6 +73,20 @@ client.on('message', message => {
         message.channel.send(attachment);
 });
 
+ client.login(process.env.BOT_TOKEN);
 
-
-client.login(process.env.BOT_TOKEN);
+ client.on('message', message => {
+  if (!message.guild) return;
+  
+  if (message.content === '/join') {
+   if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+       .then(connection => {
+          message.reply('I have successfully connected to the channel!');
+        })
+         .catch(console.log);
+   } else {
+     message.reply('You need to join a voice channel first!');
+    }
+  }
+});
