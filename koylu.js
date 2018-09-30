59f2ced50a88fdca741724930ede57705ec5fbae
Lenarch <43513762+Lenarch@users.noreply.github.com>
2018-09-30 15:06:29 +0300

    Update koylu.js

--- koylu.js
+++ koylu.js
@@ -67,13 +67,5 @@ client.on('message', message => {
     }
   }
 });
-client.on('message', message => {
-     if (message.content === 'rip') {
-        const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
-        message.channel.send(attachment);
 
-      if (message.content === 'yia') {
-        const attachment = new Attachment('https://i.hizliresim.com/1EXGEp.jpg');
-        message.channel.send(attachment);
-       
 client.login(process.env.BOT_TOKEN);
