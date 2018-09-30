20c5ccbdac326652f132cca1ea1d01b867c7ebda
Lenarch <43513762+Lenarch@users.noreply.github.com>
2018-09-30 15:17:33 +0300

    Update koylu.js

--- koylu.js
+++ koylu.js
@@ -67,5 +67,21 @@ client.on('message', message => {
     }
   }
 });
+const { Client, RichEmbed } = require('discord.js')
+const client = new Client();
+client.on('message', message => {
+  if (message.content === 'how to embed') {
+    const embed = new RichEmbed()
+ 
+      .setTitle('A slick little embed')
+ 
+      .setColor(0xFF0000)
+
+      .setDescription('Hello, this is a slick embed!');
+
+    message.channel.send(embed);
+  }
+});
+
 
 client.login(process.env.BOT_TOKEN);
