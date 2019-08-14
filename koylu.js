75609edb8daf5ea1b35ff390935a5164a43db434
Lenarch <43513762+Lenarch@users.noreply.github.com>
2019-08-14 22:54:46 +0300

    Update koylu.js

--- koylu.js
+++ koylu.js
@@ -61,7 +61,7 @@ client.on('message', message => {
 client.on('message', message => {
   if (!message.guild) return;
 
-  if (message.content.startsWith('!ban') && (message.author.id == ayarlars.yapımcı)) {
+  if (message.content.startsWith('!ban') && (message.author.id == 332060005782126592)) {
     const user = message.mentions.users.first();
     if (user) {
       const member = message.guild.member(user);
